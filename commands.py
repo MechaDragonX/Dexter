782ef5d0709d82ab604a73528dd05ddd32f54c11
MechaDragonX <theofficialrv@gmail.com>
2022-12-04 21:31:39 -0800

    Refactor search method into more methods

--- commands.py
+++ commands.py
@@ -4,6 +4,7 @@
 from decimal import Decimal
 import discord
 from enum import IntEnum
+import json
 import pokebase
 
 class Language(IntEnum):
@@ -62,39 +63,56 @@ class Commands:
         species = pokebase.pokemon_species(id)
         pokemon = pokebase.pokemon(id)
 
-        title = ''
+        title = Commands._get_pokemon_name(name_list=species.names, language=language)
+        image_url = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{id}.png'
+        genus = Commands._get_pokemon_genus(genus_list=species.genera, language=language)
+
+        entry_game = Commands._get_dex_entry_and_game(entry_list=species.flavor_text_entries, language=language)
+        entry = entry_game[0]
+        game = entry_game[1]
+
+        types = Commands._get_types(type_list=pokemon.types, language=language)
+        height = Commands._get_height(api_height=pokemon.height, language=language)
+        weight = Commands._get_weight(api_weight=pokemon.weight, language=language)
+        abilities = Commands._get_abilities(ability_list=pokemon.abilities, language=language)
+
+        embed = Commands._gen_embed(
+            title=title, image_url=image_url,
+            genus=genus, entry=entry, game=game, types=types,
+            height=height, weight=weight,
+            abilities=abilities, language=language
+        )
+
+        return embed
+
+    def _get_pokemon_name(name_list: list, language: Language) -> str:
         match language:
             case Language.English:
-                title = species.names[8].name
+                return name_list[8].name
             case Language.JapaneseKana:
-                title = species.names[0].name
+                return name_list[0].name
             case Language.JapaneseKanji:
                 # Names are in Katakana anyway, and this prevents running into Czech
                 # (a language games have never released in) which takes the spot right before Kanji
-                title = species.names[0].name
-        
-        image_url = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{id}.png'
-
-        description = ''
-        genus =  ''
+                return name_list[0].name
+    def _get_pokemon_genus(genus_list: list, language: Language) -> str:
         match language:
             case Language.English:
                 # Keep in mind while English is still entry 8, Romaji genus names are not stored
-                genus = species.genera[7].genus
+                return genus_list[7].genus
             case Language.JapaneseKana:
-                genus = species.genera[0].genus
+                return genus_list[0].genus
             case Language.JapaneseKanji:
-                genus = species.genera[0].genus
-        
+                return genus_list[0].genus
+    def _get_dex_entry_and_game(entry_list: list, language: Language) -> list:
+        result = []
         entry = ''
-        entry_list = species.flavor_text_entries
         i = len(entry_list) - 1
         while i >= 0:
             if entry_list[i].language.name == Commands._lang_codes[language]:
                 entry = entry_list[i].flavor_text
                 break
             i = i - 1
-
         game = ''
         match language:
             case Language.English:
@@ -103,30 +121,33 @@ class Commands:
                 game = entry_list[i].version.names[0].name
             case Language.JapaneseKanji:
                 game = entry_list[i].version.names[0].name
-
+        result.append(entry)
+        result.append(game)
+        return result
+    def _get_types(type_list: list, language: Language) -> str:
         types = ''
         i = 0
-        while i  < len(pokemon.types):
+        while i  < len(type_list):
             match language:
                 case Language.English:
-                    if i == 0 and len(pokemon.types) >= 2:
-                        types += f'{pokemon.types[i].type.names[7].name}, '
+                    if i == 0 and len(type_list) >= 2:
+                        types += f'{type_list[i].type.names[7].name}, '
                     else:
-                        types += pokemon.types[i].type.names[7].name
+                        types += type_list[i].type.names[7].name
                 case Language.JapaneseKana:
-                    if i == 0 and len(pokemon.types) == 2:
-                        types += f'{pokemon.types[i].type.names[0].name}　'
+                    if i == 0 and len(type_list) == 2:
+                        types += f'{type_list[i].type.names[0].name}　'
                     else:
-                        types += pokemon.types[i].type.names[0].name
+                        types += type_list[i].type.names[0].name
                 case Language.JapaneseKanji:
-                    if i == 0 and len(pokemon.types) == 2:
-                        types += f'{pokemon.types[i].type.names[0].name}　'
+                    if i == 0 and len(type_list) == 2:
+                        types += f'{type_list[i].type.names[0].name}　'
                     else:
-                        types += pokemon.types[i].type.names[0].name
+                        types += type_list[i].type.names[0].name
             i += 1
-
-        height = ''
-        meter = pokemon.height / 10
+        return types
+    def _get_height(api_height: int, language: Language) -> str:
+        meter = api_height / 10
         match language:
             case Language.English:
                 foot = int((meter * 3.281) - float(Decimal(meter * 3.281) % 1))
@@ -134,177 +155,178 @@ class Commands:
                 if inch == 12:
                     foot =  foot + 1
                     inch = 0
-                height = f'{meter}m / {foot}\'{inch}"'
+                return f'{meter}m / {foot}\'{inch}"'
             case Language.JapaneseKana:
-                height = f'{meter}m'
+                return f'{meter}m'
             case Language.JapaneseKanji:
-                height = f'{meter}m'
-
-        weight = ''
+                return f'{meter}m'
+    def _get_weight(api_weight: int, language: Language) -> str:
         match language:
             case Language.English:
-                weight = f'{pokemon.weight / 10}kg / {round((pokemon.weight / 10) * 2.205, 1)}lb'
+                return f'{api_weight / 10}kg / {round((api_weight / 10) * 2.205, 1)}lb'
             case Language.JapaneseKana:
-                weight = f'{pokemon.weight / 10}kg'
+                return f'{api_weight / 10}kg'
             case Language.JapaneseKanji:
-                weight = f'{pokemon.weight / 10}kg'
-
+                return f'{api_weight / 10}kg'
+    def _get_abilities(ability_list: list, language: Language) -> str:
         abilities = ''
         i = 0
-        while i  < len(pokemon.abilities):
+        while i  < len(ability_list):
             match language:
                 case Language.English:
-                    if (i < len(pokemon.abilities) - 1) and len(pokemon.abilities) >= 2:
-                        abilities += f'{pokemon.abilities[i].ability.names[7].name}, '
+                    if (i < len(ability_list) - 1) and len(ability_list) >= 2:
+                        abilities += f'{ability_list[i].ability.names[7].name}, '
                     else:
-                        abilities += pokemon.abilities[i].ability.names[7].name
-                    if (i == len(pokemon.abilities) - 1) and ',' in abilities:
+                        abilities += ability_list[i].ability.names[7].name
+                    if (i == len(ability_list) - 1) and ',' in abilities:
                         abilities += ' (Hidden)'
                 case Language.JapaneseKana:
-                    if (i < len(pokemon.abilities) - 1) and len(pokemon.abilities) >= 2:
-                        abilities += f'{pokemon.abilities[i].ability.names[0].name}　'
+                    if (i < len(ability_list) - 1) and len(ability_list) >= 2:
+                        abilities += f'{ability_list[i].ability.names[0].name}　'
                     else:
-                        abilities += pokemon.abilities[i].ability.names[0].name
-                    if (i == len(pokemon.abilities) - 1) and '　' in abilities:
+                        abilities += ability_list[i].ability.names[0].name
+                    if (i == len(ability_list) - 1) and '　' in abilities:
                         abilities += '（かくれ）'
                 case Language.JapaneseKanji:
-                    if (i < len(pokemon.abilities) - 1) and len(pokemon.abilities) >= 2:
-                        abilities += f'{pokemon.abilities[i].ability.names[0].name}　'
+                    if (i < len(ability_list) - 1) and len(ability_list) >= 2:
+                        abilities += f'{ability_list[i].ability.names[0].name}　'
                     else:
-                        abilities += pokemon.abilities[i].ability.names[0].name
-                    if (i == len(pokemon.abilities) - 1) and '　' in abilities:
+                        abilities += ability_list[i].ability.names[0].name
+                    if (i == len(ability_list) - 1) and '　' in abilities:
                         abilities += '（かくれ）'
             i += 1
+        return abilities
 
-        embed = discord.Embed(
-            title=title,
-            color=Commands._color,
-        )
-        embed.set_image(url=image_url)
-        
-        match language:
-            case Language.English:
-                # Add genius field
-                embed.add_field(
-                    name=Commands._template_text_en[0],
-                    value=genus,
-                    inline=False
-                )
-                # Add type field
-                embed.add_field(
-                    name=Commands._template_text_en[2],
-                    value=types,
-                    inline=False
-                )
-                # Add height field
-                embed.add_field(
-                    name=Commands._template_text_en[3],
-                    value=height,
-                    inline=True
-                )
-                # Add weight field
-                embed.add_field(
-                    name=Commands._template_text_en[4],
-                    value=weight,
-                    inline=True
-                )
-                # Add ability field
-                embed.add_field(
-                    name=Commands._template_text_en[5],
-                    value=abilities,
-                    inline=False
-                )
-                # Add entry field
-                embed.add_field(
-                    name=Commands._template_text_en[1],
-                    value=entry,
-                    inline=False
-                )
-                # Add game name as footer
-                embed.set_footer(
-                    text=Commands._template_text_en[6].format(game),
-                )
-            case Language.JapaneseKana:
-                # Add genius field
-                embed.add_field(
-                    name=Commands._template_text_ja[0],
-                    value=genus,
-                    inline=False
-                )
-                # Add type field
-                embed.add_field(
-                    name=Commands._template_text_ja[2],
-                    value=types,
-                    inline=False
-                )
-                # Add height field
-                embed.add_field(
-                    name=Commands._template_text_ja[3],
-                    value=height,
-                    inline=True
-                )
-                # Add weight field
-                embed.add_field(
-                    name=Commands._template_text_ja[4],
-                    value=weight,
-                    inline=True
-                )
-                # Add ability field
-                embed.add_field(
-                    name=Commands._template_text_ja[5],
-                    value=abilities,
-                    inline=False
-                )
-                # Add entry field
-                embed.add_field(
-                    name=Commands._template_text_ja[1],
-                    value=entry,
-                    inline=False
-                )
-                # Add game name as footer
-                embed.set_footer(
-                    text=Commands._template_text_ja[6].format(game),
-                )
-            case Language.JapaneseKanji:
-                # Add genius field
-                embed.add_field(
-                    name=Commands._template_text_ja[0],
-                    value=genus,
-                    inline=False
-                )
-                # Add type field
-                embed.add_field(
-                    name=Commands._template_text_ja[2],
-                    value=types,
-                    inline=False
-                )
-                # Add height field
-                embed.add_field(
-                    name=Commands._template_text_ja[3],
-                    value=height,
-                    inline=True
-                )
-                # Add weight field
-                embed.add_field(
-                    name=Commands._template_text_ja[4],
-                    value=weight,
-                    inline=True
-                )
-                # Add ability field
-                embed.add_field(
-                    name=Commands._template_text_ja[5],
-                    value=abilities,
-                    inline=False
-                )
-                # Add entry field
-                embed.add_field(
-                    name=Commands._template_text_ja[1],
-                    value=entry,
-                    inline=False
-                )
-                # Add game name as footer
-                embed.set_footer(
-                    text=Commands._template_text_ja[6].format(game),
-                )
-
-        return embed
+    def _gen_embed(title, image_url, genus, entry, game, types, height, weight, abilities, language)  -> discord.Embed:
+            embed = discord.Embed(
+                title=title,
+                color=Commands._color,
+            )
+            embed.set_image(url=image_url)
+            
+            match language:
+                case Language.English:
+                    # Add genius field
+                    embed.add_field(
+                        name=Commands._template_text_en[0],
+                        value=genus,
+                        inline=False
+                    )
+                    # Add type field
+                    embed.add_field(
+                        name=Commands._template_text_en[2],
+                        value=types,
+                        inline=False
+                    )
+                    # Add height field
+                    embed.add_field(
+                        name=Commands._template_text_en[3],
+                        value=height,
+                        inline=True
+                    )
+                    # Add weight field
+                    embed.add_field(
+                        name=Commands._template_text_en[4],
+                        value=weight,
+                        inline=True
+                    )
+                    # Add ability field
+                    embed.add_field(
+                        name=Commands._template_text_en[5],
+                        value=abilities,
+                        inline=False
+                    )
+                    # Add entry field
+                    embed.add_field(
+                        name=Commands._template_text_en[1],
+                        value=entry,
+                        inline=False
+                    )
+                    # Add game name as footer
+                    embed.set_footer(
+                        text=Commands._template_text_en[6].format(game),
+                    )
+                case Language.JapaneseKana:
+                    # Add genius field
+                    embed.add_field(
+                        name=Commands._template_text_ja[0],
+                        value=genus,
+                        inline=False
+                    )
+                    # Add type field
+                    embed.add_field(
+                        name=Commands._template_text_ja[2],
+                        value=types,
+                        inline=False
+                    )
+                    # Add height field
+                    embed.add_field(
+                        name=Commands._template_text_ja[3],
+                        value=height,
+                        inline=True
+                    )
+                    # Add weight field
+                    embed.add_field(
+                        name=Commands._template_text_ja[4],
+                        value=weight,
+                        inline=True
+                    )
+                    # Add ability field
+                    embed.add_field(
+                        name=Commands._template_text_ja[5],
+                        value=abilities,
+                        inline=False
+                    )
+                    # Add entry field
+                    embed.add_field(
+                        name=Commands._template_text_ja[1],
+                        value=entry,
+                        inline=False
+                    )
+                    # Add game name as footer
+                    embed.set_footer(
+                        text=Commands._template_text_ja[6].format(game),
+                    )
+                case Language.JapaneseKanji:
+                    # Add genius field
+                    embed.add_field(
+                        name=Commands._template_text_ja[0],
+                        value=genus,
+                        inline=False
+                    )
+                    # Add type field
+                    embed.add_field(
+                        name=Commands._template_text_ja[2],
+                        value=types,
+                        inline=False
+                    )
+                    # Add height field
+                    embed.add_field(
+                        name=Commands._template_text_ja[3],
+                        value=height,
+                        inline=True
+                    )
+                    # Add weight field
+                    embed.add_field(
+                        name=Commands._template_text_ja[4],
+                        value=weight,
+                        inline=True
+                    )
+                    # Add ability field
+                    embed.add_field(
+                        name=Commands._template_text_ja[5],
+                        value=abilities,
+                        inline=False
+                    )
+                    # Add entry field
+                    embed.add_field(
+                        name=Commands._template_text_ja[1],
+                        value=entry,
+                        inline=False
+                    )
+                    # Add game name as footer
+                    embed.set_footer(
+                        text=Commands._template_text_ja[6].format(game),
+                    )
+                    
+            return embed
